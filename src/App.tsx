28722c73ca27f17d5efb20062316db9d80d41349
kozervar <kozervar@gmail.com>
2018-01-30 23:54:39 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="hello-world">
+                <h1>Hello world!</h1>
+            </div>
+        );
+    }
+
+}
+
+export default App;
